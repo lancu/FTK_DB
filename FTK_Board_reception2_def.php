91896d971fd114296507e54e4c5577edc2ea6387
lucian ancu <lucian@cern.ch>
2014-11-24 20:22:13 +0100

    Fixed the "10"

--- FTK_Board_reception2_def.php
+++ FTK_Board_reception2_def.php
@@ -34,7 +34,7 @@ $row = mysqli_fetch_array($r);
 
 $enumList = explode(",", str_replace("'", "", substr($row['COLUMN_TYPE'], 5, (strlen($row['COLUMN_TYPE'])-6))));   
 
-    print(count($enumList));
+   // print(count($enumList));
     $array = array(
         "foo" => "bar",
         "bar" => "foo");
@@ -250,9 +250,9 @@ mysqli_close($dbc);
  //STARTING FTK MESS
   --> 
 
-
 <form enctype="multipart/form-data" method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
     <input type="hidden" name="MAX_FILE_SIZE" value="<?php echo MM_MAXFILESIZE; ?>" />    
+
 
     <fieldset><legend>FTK entry Information </legend>
       <fieldset><legend>Information on the user</legend>
