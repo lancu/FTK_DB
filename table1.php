d81bef484399fb5b984e55da630088e07a4be052
picazio <attilio.picazio@gmail.com>
2014-12-01 12:51:16 +0100

    login added and remove button

--- table1.php
+++ table1.php
@@ -1,6 +1,44 @@
-//new table form
 <?php
-mysql_connect('localhost','root','root') or die(mysql_error());
+  session_start();
+
+  // If the session vars aren't set, try to set them with a cookie
+  if (!isset($_SESSION['user_id'])) {
+    if (isset($_COOKIE['user_id']) && isset($_COOKIE['username'])) {
+      $_SESSION['user_id'] = $_COOKIE['user_id'];
+      $_SESSION['username'] = $_COOKIE['username'];
+    }
+  }
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <title>FTK Production Database Interface - View Boad Data</title>
+  <link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+<body>
+  <h3>FTK Production Database Interface - View Board Data</h3>
+  <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+<?php
+  require_once('appvars.php');
+  require_once('connectvars.php');
+
+  // Make sure the user is logged in before going any further.
+  if (!isset($_SESSION['user_id'])) {
+    echo '<p class="login">Please <a href="login.php">log in</a> to access this page.</p>';
+    exit();
+  }
+  else {
+    echo('<p class="login">You are logged in as ' . $_SESSION['username'] . '. <a href="logout.php">Log out</a>.<br /> Go back to the <a href="index.php">Index</a>.</p>');
+  }
+
+
+//<?php
+mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME)
+//mysql_connect('localhost','root','root') 
+    or die(mysql_error());
 mysql_select_db('Test') or die(mysql_error());
 $field='board_type';
 $sort='ASC';
@@ -81,15 +119,38 @@ elseif($_GET['field']=='Status')
 }
 
 
-
-
 //$sql = "SELECT board_type, board_id, inst_status FROM FTK_parts ORDER BY $field $sort";
 $sql = "SELECT * FROM FTK_parts ORDER BY $field $sort";
 //$sql = "SELECT board_type, board_id, inst_status FROM FTK_parts ORDER BY id $sort";
 $result = mysql_query($sql) or die(mysql_error());
+
+ if (isset($_POST['submit'])) {
+    foreach ($_POST['todelete'] as $delete_id) {
+      $query = "DELETE FROM FTK_parts WHERE id = $delete_id";
+      //echo $query;
+      //echo '<br />';
+      
+      //mysqli_query($dbc, $query)
+      mysql_query($query)
+        or die('Error querying database.');
+    } 
+    
+    echo 'Module(s) removed.<br />';
+    }
+
 echo'<table border="1">';
-echo'<th><a href="table1.php?sorting='.$sort.'&field=board_type">Board Type</a></th>
-     <th><a href="table1.php?sorting='.$sort.'&field=board_id">Board ID</a></th>
+if($_SESSION['username']==ADMIN || $_SESSION['username']==ADMIN2)
+  { 
+    //echo 'strange mode';
+    //echo '<tr><td> Remove</td>';
+        echo'<th>Remove</th><th><a href="table1.php?sorting='.$sort.'&field=board_type">Board Type</a></th>';
+    }
+else 
+    {   
+    echo'<th><a href="table1.php?sorting='.$sort.'&field=board_type">Board Type</a></th>';
+
+    }
+echo'<th><a href="table1.php?sorting='.$sort.'&field=board_id">Board ID</a></th>
      <th><a href="table1.php?sorting='.$sort.'&field=location">location</a></th>
      <th><a href="table1.php?sorting='.$sort.'&field=rack">rack</a></th>
      <th><a href="table1.php?sorting='.$sort.'&field=crate">crate</a></th>
@@ -107,10 +168,22 @@ echo'<th><a href="table1.php?sorting='.$sort.'&field=board_type">Board Type</a><
 <th><a href="table1.php?sorting='.$sort.'&field=Status">Status</a></th>
 
 <th><a href="table1.php?sorting='.$sort.'&field=board_type_ID">Board type ID</a></th>';
+ 
 while($row = mysql_fetch_array($result))
 {
-echo'<tr><td>'.$row['board_type'].'</td>
-        <td>'.$row['board_id'].'</td>
+    
+    if($_SESSION['username']==ADMIN || $_SESSION['username']==ADMIN2)
+        {
+            echo'<tr><td><input type="checkbox" value="' . $row['id'] . '" name="todelete[]" /></td>';                               
+            echo'<td>'.$row['board_type'].'</td>';
+        }
+    else
+        {
+            echo'<tr><td>'.$row['board_type'].'</td>';
+        }
+
+
+    echo'<td>'.$row['board_id'].'</td>
         <td>'.$row['location'].'</td>
         <td>'.$row['rack'].'</td>
          <td>'.$row['crate'].'</td>
@@ -129,13 +202,23 @@ echo'<tr><td>'.$row['board_type'].'</td>
         <td>'.$row['Status'].'</td>
         <td>'.$row['board_type_ID'].'</td>
     
-    </tr>'
-    
-    
-    
-    ;
+    </tr>';
 }
 
 echo'</table>';
+
+
+mysqli_close($dbc);
 ?>
-// end table       
+      
+<?php 
+
+if($_SESSION['username']==ADMIN || $_SESSION['username']==ADMIN2){ 
+  echo '<input type="submit" name="submit" value="Remove" />';
+ }
+ else{ 
+   echo '<input type="hidden" name="submit" value="Remove" />';
+     }
+?>      
+      
+<!--// end table -->       
